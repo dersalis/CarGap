ed66c5cab44dcd482ab846ba51a65f5198f8dc88
Damian <hello@damianruta.pl>
2022-11-14 20:59:55 +0100

    Initial Commit

---
+++ CarGap/CarGapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarGapApp.swift
+//  CarGap
+//
+//  Created by Damian Ruta on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarGapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
